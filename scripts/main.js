5fc2362f345f704da293a78c40c8ad5eaf18c348
kamilabartnik <kbartnik@g.pl>
2019-06-10 13:30:42 +0200

    add name

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,29 @@
 let myImagine = document.querySelector('img');
+let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('Please enter your name');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool ' +myName
+}
 
 myImagine.onclick = function() {
     let mySrc = myImagine.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png'){
         myImagine.setAttribute('src', 'images/cat.png');
-        myHeading.textContent = 'Mozilla is cool';
     } else {
         myImagine.setAttribute('src', 'images/firefox-icon.png');
-        myHeading.textContent = 'Mozilla is not cool';
     }
 }
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
