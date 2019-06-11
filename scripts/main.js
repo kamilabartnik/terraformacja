b2fa3d12ee7dd293c02f9b495a0444a0fd1da66e
kamilabartnik <kbartnik@g.pl>
2019-06-11 14:08:54 +0200

    sidebar

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,8 @@
-let myImagine = document.querySelector('img');
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
 
-function setUserName(){
-    let myName = prompt('Please enter your name');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Terraformacja jest super, ' +myName
-}
 
-// myImagine.onclick = function() {
-//     let mySrc = myImagine.getAttribute('src');
-//     if(mySrc === 'images/uku.jpeg'){
-//         myImagine.setAttribute('src', 'images/cat.png');
-//     } else {
-//         myImagine.setAttribute('src', 'images/uku.jpeg');
-//     }
-// }
+const button = document.querySelector('button');
 
-// if(!localStorage.getItem('name')) {
-//     setUserName();
-//   } else {
-//     var storedName = localStorage.getItem('name');
-//     myHeading.textContent = 'Terraformacja jest super, ' + storedName;
-//   }
+button.addEventListener('click', _ => {
+  document.getElementById('sidebar').classList.toggle('collapsed');
+})
 
-//   myButton.onclick = function() {
-//     setUserName();
-//   }
