85bbda862f57a2cff6abd513bc1bd5e127df6bc4
kamilabartnik <kbartnik@g.pl>
2019-06-11 15:24:26 +0200

    menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,17 @@
-
-
 const button = document.querySelector('button');
 
 button.addEventListener('click', _ => {
   document.getElementById('sidebar').classList.toggle('collapsed');
 })
 
+$(document).ready(function(){
+    $(window).bind('scroll', function() {
+    var navHeight = $( window ).height() - 70;
+          if ($(window).scrollTop() > navHeight) {
+              $('nav').addClass('fixed');
+          }
+          else {
+              $('nav').removeClass('fixed');
+          }
+     });
+ });
