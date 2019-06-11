97a450abded11bc6fe9767bcf28f2fa7b8e61a09
kamilabartnik <kbartnik@g.pl>
2019-06-11 11:08:09 +0200

    terra

--- scripts/main.js
+++ scripts/main.js
@@ -5,25 +5,25 @@ let myHeading = document.querySelector('h1');
 function setUserName(){
     let myName = prompt('Please enter your name');
     localStorage.setItem('name', myName);
-    myHeading.textContent = 'Ukulele is cool, ' +myName
+    myHeading.textContent = 'Terraformacja jest super, ' +myName
 }
 
-myImagine.onclick = function() {
-    let mySrc = myImagine.getAttribute('src');
-    if(mySrc === 'images/uku.jpeg'){
-        myImagine.setAttribute('src', 'images/cat.png');
-    } else {
-        myImagine.setAttribute('src', 'images/uku.jpeg');
-    }
-}
+// myImagine.onclick = function() {
+//     let mySrc = myImagine.getAttribute('src');
+//     if(mySrc === 'images/uku.jpeg'){
+//         myImagine.setAttribute('src', 'images/cat.png');
+//     } else {
+//         myImagine.setAttribute('src', 'images/uku.jpeg');
+//     }
+// }
 
-if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Ukulele is cool, ' + storedName;
-  }
+// if(!localStorage.getItem('name')) {
+//     setUserName();
+//   } else {
+//     var storedName = localStorage.getItem('name');
+//     myHeading.textContent = 'Terraformacja jest super, ' + storedName;
+//   }
 
-  myButton.onclick = function() {
-    setUserName();
-  }
+//   myButton.onclick = function() {
+//     setUserName();
+//   }
