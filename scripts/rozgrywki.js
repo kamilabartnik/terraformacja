41d3fc25a7716c378f71e2cece8d7489d7781d6c
kamilabartnik <kbartnik@g.pl>
2019-06-23 11:33:10 +0200

    form text

---
+++ scripts/rozgrywki.js
@@ -0,0 +1,21 @@
+const App = new Vue ({
+
+    el:"formcorrect",
+
+    data: {
+        form: {
+            place:null,
+            date:null,
+            winner:null,
+            second:null,
+            third:null,
+            forth:null,
+            fifth:null,
+            generation:null
+        }
+    },
+
+    mounted(){
+        // alert('yes');
+    }
+})
